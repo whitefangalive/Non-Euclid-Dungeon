b727b2b493456c63c46513222b7e712d06d66e7f
Whike <49380278+whitefangalive@users.noreply.github.com>
2024-03-14 13:06:45 -0500

    backpack bug fixes

--- Assets/FollowHand.cs
+++ Assets/FollowHand.cs
@@ -8,11 +8,13 @@ public class FollowHand : MonoBehaviour
     public GameObject player;
     private Vector3 offset;
     public float smoothSpeed = 0.125f;
+    private Transform forwardOnY;
     // Start is called before the first frame update
     void Start()
     {
         //Initializes the offset to be equal to camera distance from player
         offset = transform.position - player.transform.position;
+        forwardOnY = player.transform;
     }
 
     // Update is called once per frame
@@ -20,9 +22,10 @@ public class FollowHand : MonoBehaviour
     {
 
         //Updates camera position to be position of the player plus the initial offset.
-        Vector3 direction = Vector3.Scale(player.transform.TransformDirection(Vector3.forward), new Vector3(1, 0, 1));
-        Ray r = new Ray(player.transform.position, direction);
-        Debug.DrawRay(player.transform.position, direction * offset.z);
+        forwardOnY.rotation = new Quaternion(0, player.transform.rotation.y, 0, player.transform.rotation.w);
+        
+        Ray r = new Ray(player.transform.position, forwardOnY.TransformDirection(Vector3.forward));
+        Debug.DrawRay(player.transform.position, forwardOnY.TransformDirection(Vector3.forward) * offset.z);
 
         Vector3 desiredPosition = r.GetPoint(offset.z);
         Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
